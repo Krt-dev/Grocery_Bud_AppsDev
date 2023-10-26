b832707495d30802162b9e4ad965545c147d3f37
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2023-10-26 10:21:29 +0800

    Initial prototype complete
    design your own next

---
+++ groceryBud/src/notification.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+interface NotifyProps {
+  type: string;
+  msg: string;
+  removeAlert: () => void;
+  list: string[];
+}
+
+const Notify: React.FC<NotifyProps> = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list, removeAlert]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Notify;
